fde7043514d41a1fcbf9f0ccffbbb05fc1ea740f
YR - Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-05-27 12:51:59 -0300

    feat: create new modules and repositories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './infra/notifications.controller';
-import { PrismaService } from './infra/prisma.service';
+import { DataBaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
-  controllers: [NotificationsController],
-  providers: [PrismaService],
+  imports: [HttpModule, DataBaseModule],
 })
-export class AppModule {}
+export class AppModule { }
